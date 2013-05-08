7792bd6f95937e965b23c0b5e650989433d42273
Roland Sommer <rol@ndsommer.de>
2013-05-08 14:59:24 +0200

    Version Bump (klemmbrett 0.4)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools as _setuptools
 
 _setuptools.setup(
     name = "klemmbrett",
-    version = "0.3",
+    version = "0.4",
     description = "Versatile clipboard manager",
     author = "Michael van Bracht",
     author_email = "michael@wontfix.org",
